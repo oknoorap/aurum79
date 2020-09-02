b0d822bc340da9cbb5f20ff91d9ed72dfa47031e
Ribhararnus Pracutian <oknoorap@gmail.com>
2020-09-02 21:41:32 +0700

    fix: build script

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,2 +1,6 @@
 "C:\Program Files\MetaTrader 5\metaeditor64" /compile:"%cd%\..\src\mql\Aurum79.mq5" /log
 type "%cd%\..\src\mql\Aurum79.log"
+mkdir dist
+IF EXIST "%cd%\..\src\mql\Aurum79.ex5" (
+  xcopy "%cd%\..\src\mql\Aurum79.ex5" "%cd%\..\dist\Aurum79.ex5" /Y
+)

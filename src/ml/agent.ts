1886c6c7d4b3452997730d44ee1c092d575e4769
Ribhararnus Pracutian <oknoorap@gmail.com>
2020-11-17 08:54:41 +0700

    fix: test change activation to sigmoid

--- src/ml/agent.ts
+++ src/ml/agent.ts
@@ -124,7 +124,7 @@ class Agent {
       const outputs = <tf.SymbolicTensor>tf.layers
         .dense({
           units: 3,
-          activation: 'softmax',
+          activation: 'sigmoid',
           name: 'output',
         })
         .apply(flatten);

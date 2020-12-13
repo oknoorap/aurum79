cd1f2739d8a326d9dbd4b4a812044a02b3a255d1
Ribhararnus Pracutian <oknoorap@gmail.com>
2020-12-13 09:52:48 +0700

    fix: build dir structure

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,6 +1,6 @@
 "C:\Program Files\MetaTrader 5\metaeditor64" /compile:"%cd%\..\src\mql\Aurum79.mq5" /log
 type "%cd%\..\src\mql\Aurum79.log"
-mkdir dist
+mkdir dist\ea
 IF EXIST "%cd%\..\src\mql\Aurum79.ex5" (
-  echo f | xcopy /f /y "%cd%\..\src\mql\Aurum79.ex5" "%cd%\..\dist\Aurum79.ex5"
+  echo f | xcopy /f /y "%cd%\..\src\mql\Aurum79.ex5" "%cd%\..\dist\ea\Aurum79.ex5"
 )
